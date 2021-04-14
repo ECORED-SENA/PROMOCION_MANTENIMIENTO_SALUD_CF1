3b200265377355fccd5d6a1d6236c97dd946e7d1
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-04-14 15:46:10 -0500

    CF1 V0.1
    Tema uno listo

---
+++ src/views/Introduccion.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.curso-main-container.introduccion
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  
+    .titulo-principal
+      .titulo-principal__numero
+        .h3
+          i.fas.fa-info
+      .h3 Introducción
+    
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Leyenda del video
+    
+    p.mb-5 La Atención Integral en Salud es un concepto que organiza la acción del sector salud y otros sectores en el marco de la garantía del derecho a la salud. Las Rutas integrales de atención en salud son una herramienta que permite articular los esfuerzos de los actores propios y de otros sectores, apuntando al logro de los resultados en salud ubicados en los sujetos, por medio de los procesos de gestión que se vinculan a escenarios de atención que potencian la acción organizada y promueven la capacidad de agencia en las personas, familias y comunidades.
+      br
+      br
+      |Este componente formativo brinda los elementos para la comprensión de la atención integral en salud, los desarrollos normativos y conceptuales que le son propios, haciendo énfasis en las Rutas Integrales de Atención en Salud como una apuesta del sector y en particular en la Ruta Integral de Atención para la Promoción y Mantenimiento de la Salud.
+
+</template>
+
+<script>
+import BannerInterno from '../components/plantilla/BannerInterno'
+export default {
+  name: 'Introduccion',
+  components: {
+    BannerInterno,
+  },
+  data: () => ({
+    globalData: global,
+  }),
+}
+</script>
+
+<style lang="sass" scoped>
+.introduccion
+</style>
---
+++ src/views/curso/Tema2.vue
@@ -0,0 +1,147 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 2
+      .h3 Generalidades y principios
+    p.mb-5 Son una herramienta de obligatorio cumplimiento, establecidas en el marco de la Política PAIS y la Resolución 3202 de 2016, que contribuyen a que las personas, familias y comunidades a que alcancen resultados en salud teniendo en cuenta las particularidades del territorio donde viven, se relacionan y se desarrollan. Permiten orientar y ordenar la gestión de la atención integral en salud a cargo de los actores del Sistema General  de Seguridad Social en Salud -SGSSS- de manera continua y consecuente con la situación y características de cada territorio, así como la gestión intersectorial para la afectación de los determinantes sociales de la salud. Integran además las atenciones individuales, colectivas y poblacionales para la promoción de la salud, prevención de la enfermedad, diagnóstico, tratamiento, rehabilitación y paliación y plantea las acciones de cuidado que se esperan del individuo para el mantenimiento o recuperación de su salud.
+    figure.mb-5
+      img(src='@/assets/curso/img8.svg', alt='Texto que describa la imagen')
+    .h4.titulo-rosa Postulados orientadores de las Rutas Integrales de Atención en Salud (RIAS)
+    .row
+      .col-lg-6
+        ul.lista-ul--color
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Son una herramienta centrada en las personas que ubica en un lugar estratégico el ejercicio del derecho a la salud.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Están orientadas a incidir en los resultados en salud de impacto e intermedios, así como en la calidad de la atención.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Definen la gestión y atenciones requeridas para anticiparse a la contingencia con atenciones de promoción de la salud. 
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Permite pasar de un abordaje disciplinar a un abordaje multidisciplinar y de un abordaje fragmentado a un abordaje integral de los sujetos. 
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Reconoce la familia como sujeto de atención en salud y tiene en cuenta el contexto familiar en la salud de las personas. Así mismo reconoce que estos hacen parte de una comunidad.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2.mb-md-5.mb-lg-0 Plantea intervenciones integrales, complementarias y anticipatorias, que contribuyan a la garantía de condiciones para el desarrollo humano, teniendo en cuenta las ventanas de oportunidad en el desarrollo integral de las personas en sus diferentes momentos del curso de la vida.
+      .col-lg-6.align-self-end
+        figure
+          img.img-col(src='@/assets/curso/img9.svg', alt='Texto que describa la imagen')
+    .row.my-5
+      .col-lg-6.order-lg-0.order-md-1.order-sm-1
+        figure
+          img.img-col.mt-sm-5(src='@/assets/curso/img10.svg', alt='Texto que describa la imagen')
+      .col-lg-6
+        ul.lista-ul--color
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Logra la complementariedad de la atención desde la baja complejidad hasta la alta complejidad.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Reorganiza las respuestas programáticas en función de la atención integral en salud y los sujetos.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2 Ubican los entornos como escenarios de transformación social que puedan potenciarse como protectores y promotores de la salud y el desarrollo humano, así como en unidades de gestión donde se pueden materializar las atenciones (individuales, colectivas y poblacionales)
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p.mx-2.mb-md-5.mb-lg-0 La operación de las RIAS reconoce el 
+              b territorio como unidad base 
+              b-none donde se integra el sistema de salud y los agentes responsables de garantizar la atención integral en salud.
+    .titulo-segundo
+      .h4  2.2 Ordenadores de las RIA
+    .h4.titulo-rosa La resolución 3202 de 2016 define 3 tipos de RIAS
+    ol.lista-ol--cuadro
+          li 
+            .lista-ol--cuadro__vineta
+              span a
+            p 
+              b Ruta integral de atención en salud para la promoción y mantenimiento de la salud 
+              b-none (RPMS) Centrada en la promoción de la salud, la prevención del riesgo y la enfermedad; así como la generación de una cultura del cuidado de la salud en las personas, familias y comunidades. Aplica para toda la población residente en el territorio.
+    div.recuadro-rosa.my-5
+      div.cuadro-texto_azul.mt-5
+        p.text-center Fisico y Motor / Socio - emocional / Cognitivo
+      div.circulo-numero
+        h4 1
+      p.text-center 
+        b Ruta integral de atención en salud para la promoción y mantenimiento de la salud 
+      p.text-center.mb-3 Acciones realizadas en los entornos donde transcurre la vida y se desarrollan las personas
+      figure.mb-3
+          img(src='@/assets/curso/img12.svg', alt='Texto que describa la imagen')
+    ol.lista-ol--cuadro
+          li 
+            .lista-ol--cuadro__vineta
+              span b
+            p 
+              b Rutas Integrales de Atención en Salud para grupos de riesgo.
+              b-none  Tienen el objetivo de  identificar e intervenir oportunamente factores de riesgo, ya sea para evitar la aparición de una o varias condiciones específicas en salud o para realizar el diagnóstico, tratamiento, rehabilitación y paliación, según cada situación. 
+    div.recuadro-rosa.my-5
+      div.circulo-numero
+        h4 2
+      p.text-center 
+        b Ruta integral de atención en salud para grupos de riesgo.
+      p.text-center.mb-3 Acciones dirigidas a la identificación oportuna de factores de riesgo y su intervención.
+      figure.mb-3
+          img(src='@/assets/curso/img13.svg', alt='Texto que describa la imagen')
+    p.mb-5 Esta ruta aplica para la población en riesgo residente en el territorio, ya sea detectada en el marco de la RPMS, en el marco de otra Ruta de Riesgo o por demanda espontánea. La RIAS para población materno – perinatal (RIAMP) hace parte de este grupo de RIAS de riesgo, aunque se considera totalmente vinculada a la RPMS y es un “bucle” de la atención en el marco de la RPMS. 
+      br
+      br
+      | En este grupo se encuentran las 16 RIAS para población con riesgo o presencia de: i) alteraciones cardio – cerebro – vascular – metabólicas manifiestas*; ii) enfermedades respiratorias crónicas; iii) alteraciones nutricionales*; iv) trastornos mentales y del comportamiento manifiestos debido a uso de sustancias psicoactivas y adicciones*; v) trastornos psicosociales y del comportamiento; vi) alteraciones en la salud bucal; vii) cáncer*; viii) enfermedades infecciosas*; ix) zoonosis y agresiones por animales; x) enfermedad y accidentes laborales; xi) agresiones, accidentes y traumas; xii) enfermedades y accidentes relacionados con el medio ambiente; xiii) enfermedades huérfanas; xiv) trastornos visuales y auditivos; xv) trastornos degenerativos, neuropatías y autoinmunes y xvi) RIAMP.
+    ol.lista-ol--cuadro.mb-5
+          li 
+            .lista-ol--cuadro__vineta
+              span c
+            p 
+              b Ruta integral de atención en salud para eventos específicos.
+              b-none  Estas rutas se definen a partir de aquellos eventos específicos priorizados por RIAS de riesgo. Se orientan al tratamiento, rehabilitación y paliación, según sea el caso.
+    .cajon--rojo.p-4.mb-5
+          p 
+            b La Resolución 3202 de 2016, 
+            b-none adoptó las siguientes RIAS y estableció que las Normas Técnicas de Detección temprana y Protección específica establecidas por Resolución 412 de 2000 serían derogadas una vez se definiera el Lineamiento técnico y operativo de la RPMS y la RIAMP.
+    .h4.titulo-rosa.text-center  Adopción de RIAS - Resolución 3202 de 2016 
+
+
+    #t_2_1.pb-5.h2.mt-5 tema 2.1
+    .pb-5(v-for="item of 15")
+
+    #t_2_2.pb-5.h2 tema 2.2
+    .pb-5(v-for="item of 15")
+
+    #t_2_3.pb-5.h2 tema 2.3
+    .pb-5(v-for="item of 15")
+
+    #t_2_4.pb-5.h2 tema 2.4
+    .pb-5(v-for="item of 15")
+
+    #t_2_5.pb-5.h2 tema 2.5
+    .pb-5(v-for="item of 15")
+
+    #t_2_6.pb-5.h2 tema 2.6
+    .pb-5(v-for="item of 15")
+
+    #t_2_7.pb-5.h2 tema 2.7
+    .pb-5(v-for="item of 15")
+
+    #t_2_8.pb-5.h2 tema 2.8
+    .pb-5(v-for="item of 15")
+
+</template>
+
+<script>
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema2',
+  components: {
+    BannerInterno,
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
