c93f93e927e2d4d978c5815fc62c2b6b5af7f965
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-04-15 10:53:41 -0500

    CF1 V0.4
    Tema tres finalizado

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -13,7 +13,7 @@
           p Las disposiciones son de obligatorio cumplimiento por parte de las entidades territoriales de salud, EPS – Entidades Promotoras de Salud, Entidades Obligadas a Compensar, prestadores de servicios de salud y demás entidades responsables de las intervenciones relacionadas con la promoción y el mantenimiento de la salud.
       .col-lg-5
         figure.p-3
-          img.img-col--2(src='@/assets/curso/img22.svg', alt='Texto que describa la imagen').img-fluid
+          img.img-col--2(src='@/assets/curso/img22.svg', alt='Texto que describa la imagen')
     .row.my-3
       .col-lg-5
       .col-lg-7
@@ -38,28 +38,28 @@
     .h4.titulo-rosa.my-5 Objetivos de la ruta de promoción y mantenimiento de la salud
     ol.lista-ol--cuadro
           li 
-            .lista-ol--cuadro__vineta
+            .lista-ol--cuadro__vineta.p-3
               span a
-            p Identificar y desarrollar capacidades, habilidades y prácticas en las personas, las familias y las comunidades para el cuidado y mantenimiento de la salud.
+            p.ms-2 Identificar y desarrollar capacidades, habilidades y prácticas en las personas, las familias y las comunidades para el cuidado y mantenimiento de la salud.
 
           li 
-            .lista-ol--cuadro__vineta
+            .lista-ol--cuadro__vineta.p-3
               span b
-            p Atender oportuna e integralmente a las personas que presentan un evento en salud. 
+            p.ms-2 Atender oportuna e integralmente a las personas que presentan un evento en salud. 
 
           li 
-            .lista-ol--cuadro__vineta
+            .lista-ol--cuadro__vineta.p-3
               span c
-            p Identificar oportunamente vulnerabilidades, factores de riesgo, riesgos acumulados y alteraciones que emergen en cada momento vital, que ameriten acciones en salud.
+            p.ms-2 Identificar oportunamente vulnerabilidades, factores de riesgo, riesgos acumulados y alteraciones que emergen en cada momento vital, que ameriten acciones en salud.
 
           li 
-            .lista-ol--cuadro__vineta
+            .lista-ol--cuadro__vineta.p-3
               span d
-            p Afectar los entornos para que sean protectores y potenciadores de la salud y el desarrollo.   
+            p.ms-2 Afectar los entornos para que sean protectores y potenciadores de la salud y el desarrollo.   
           li 
-            .lista-ol--cuadro__vineta
+            .lista-ol--cuadro__vineta.p-3
               span e
-            p Realizar el seguimiento de la salud y el desarrollo integral de las personas en cada uno de los momentos del curso de vida.
+            p.ms-2 Realizar el seguimiento de la salud y el desarrollo integral de las personas en cada uno de los momentos del curso de vida.
     .h4.titulo-rosa.my-5 La RPMS plantea cambios importantes en relación a lo planteado anteriormente en la Resolución 412 de 2000 (derogada por Resolución 3280 de 2018) como es:
     ul.lista-ul--color
           li.lista-separada 
@@ -74,6 +74,145 @@
           li.lista-separada 
             i.fas.fa-check.mx-2
             p Incluir nuevas finalidades: valoración integral en Salud y Educación en salud que se complementan con las finalidades anteriores (detección temprana y protección específica).
+    .titulo-segundo
+      .h4.mt-5  3.1. Resultados en Salud
+    p Una de las apuestas esenciales planteadas en las RIAS y que se concretan en la RPMS y RIAMP son los resultados en salud como apuesta central en la que deben combinarse los esfuerzos de todos los actores. Son los cambios en las condiciones de vida, concepciones y prácticas que promueven, mantienen y recuperan la salud a la vez que favorecen el desarrollo integral de las personas, familias y comunidades, atribuidos a las intervenciones y atenciones en el continuo de la atención integral en salud 
+    .bloque-texto-a.p-4.p-md-5.my-5
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-4.mb-4.mb-lg-0
+          .h2.mb-0 Comprenden resultados finales o de impacto y resultados intermedios o de efecto. 
+        .col-lg-7
+          .bloque-texto-a__texto.p-4
+            p 
+              b Los resultados de impacto 
+              b-none requieren de la acción sectorial e intersectorial que incide en los determinantes sociales y están relacionados con el logro de los resultados intermedios. Los indicadores de impacto se relacionan de manera especial con compromisos de los  Objetivos de Desarrollo Sostenible y tienen una fuerte relación con las posibilidades de desarrollo de un Estado.
+                //todo AGREGAR LISTA EN HTML
+    .tabla-a.mb-5 
+      table
+        thead
+          tr
+            th Primera infancia
+            th Infancia 
+            th Adolesencia 
+            th Juventud
+            th Adultez
+            th Vejez
+        tbody
+          tr.tr-center
+            <td colspan="5" rowspan="2">Personas que se desarrollan adecuadamente desde el punto de vista físico-motor, socio-emocional y cognitivo</td>
+            td Personas adultas mayores que conservan las habilidades cognitivas
+          tr.tr-center
+            td Personas adultas mayores que conservan la independencia funcional
+          tr.tr-center
+            td Niñas y niños alimentados exclusivamente con lactancia materna hasta los 6 meses
+            <td colspan="5"></td>
+          tr.tr-center
+            <td colspan="6"> Personas con adecuado estado nutriconal</td> 
+          tr.tr-center
+            <td colspan="6"> Personas sin problemas y transtornos en salud mental</td> 
+          tr.tr-center
+            <td colspan="6"> Personas sin morbilidad evitable</td> 
+          tr.tr-center
+            <td colspan="6"> Personas sin mortalidad evitable</td> 
+            
+    p Los resultados intermedios, son los cambios esperados en las personas en cada momento del curso de vida, en las familias y comunidades atribuibles a la garantía de una o varias atenciones contempladas en las RIAS y que pueden ser logrados en un corto o mediano plazo. 
+    ul.lista-ul--color.my-5
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas con adecuado estado de salud visual / auditiva y comunicativa / salud bucal.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas con detección temprana de alteraciones y riesgos en salud.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas que ejercen sus derechos sexuales y reproductivos.(detección temprana y protección específica).
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas con prácticas que favorecen el establecimiento de relaciones sanas y constructivas.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas con habilidades sociales y emocionales para la promoción de la salud mental y convivencia.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas, familias y comunidades que cuentan con redes de apoyo comunitarias y sociales para la promoción de la salud.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas, familias y comunidades con capacidad de agencia y prácticas para el cuidado de su salud.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas cuidadoras con prácticas para el cuidado de la salud y crianza promotora de la salud y del desarrollo de niñas y niños.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Trabajadores con prácticas de cuidado de su salud en el trabajo.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas, familias y comunidades que habitan, laboran y estudian en entornos saludables.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas, familias y comunidades con prácticas para el cuidado y protección de los entornos.
+          li.lista-separada 
+            i.fas.fa-check.mx-2
+            p Personas, familias y comunidades empoderadas para la exigibilidad y ejercicio del derecho a la vida y la salud.
+    p Se espera así mismo, a través de la implementación de las RIAS, que se garanticen atributos y condiciones de calidad (integralidad, accesibilidad, oportunidad, disponibilidad, continuidad) con las consideraciones de abordaje poblacional y territorial; apuntando también a reducir brechas de equidad en salud asociadas a condiciones socioeconómicas, dispersión poblacional, pertenencia étnica, entre otros diferenciales. Estos resultados en salud se materializan en la conjunción de la acción intersectorial y la gestión de intervenciones (individuales, colectivas y poblacionales) que finalmente son dispuestas a los sujetos de atención.
+    .titulo-segundo
+      .h4.mt-5  3.2. Intervenciones de la RPMS
+    .h4.titulo-rosa Poblacionales
+    p Son las intervenciones dirigidas a toda la población que aportan en los procesos sociales, ambientales, económicos que se relacionan con la calidad de vida, el bienestar, el desarrollo y la salud de las poblaciones. 
+    figure.p-3.my-5
+          img(src='@/assets/curso/img24.svg', alt='Texto que describa la imagen')
+    .h4.titulo-rosa Las líneas de acción que orientan las intervenciones poblacionales son:
+    ol.lista-ol--cuadro
+          li 
+            .lista-ol--cuadro__vineta.p-3
+              span a
+            p.ms-3 Políticas Públicas: para promover un tipo específico de actuación (por ejemplo estrategia de consumo de frutas y verduras); proteger a un sector de la sociedad frente a una amenaza (por ejemplo planes para garantizar la inocuidad de alimentos); regular intereses en conflicto (por ejemplo impuesto al tabaco); proporcionar beneficios a grupos concretos (por ejemplo políticas de subsidio); desestimular un comportamiento o práctica (por ejemplo política de seguridad vial); generar entornos protectores y promotores de la salud y el desarrollo (por ejemplo estrategias de recreación)
+
+          li 
+            .lista-ol--cuadro__vineta.p-3
+              span b
+            p.ms-3 Información en Salud: Suministrar información con énfasis en derechos y deberes en salud; promoción de prácticas para el cuidado de la salud; prevención, identificación y mitigación de riesgos, mecanismos de participación social en salud y reconocimiento de las RIAS y mecanismos para reducir las barreras de acceso a los servicios de salud.
+
+
+          li 
+            .lista-ol--cuadro__vineta.p-3
+              span c
+            p.ms-3 Participación Social: Promover escenarios de encuentro, deliberación, concertación y organización, teniendo presente los mecanismos y espacios de participación social en salud normados, así como los promovidos por otros sectores y organizaciones comunitarias de base.
+    figure.p-3.my-5
+          img(src='@/assets/curso/img25.svg', alt='Texto que describa la imagen')
+    p Intervenciones dirigidas a grupos o colectivos  de personas que apuestan por promover el desarrollo de capacidades en los sujetos, favorecer el empoderamiento de los individuos, generar entornos saludables, protectores, que favorezcan el desarrollo humano y contribuyan al  bienestar individual y colectivo, así como a  la gestión integral del riesgo en salud. Tiene como unidad de gestión los entornos: Institucional (incluye los servicios de salud como las poblaciones institucionalizadas), hogar, educativo, comunitario, laboral. 
+    .tarjeta.tarjeta--amarilla.p-3.mb-5.mx-5.my-5
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-1
+          img(src="@/assets/curso/img26.svg")
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0
+              .h5.mb-0 Intervenciones Colectivas
+              p.text-small Son en total 11 tecnologías que se pueden incorporar en el Plan de Salud Pública de Intervenciones Colectivas (PSPIC) de cada territorio (municipio, distrito y departamento). 
+
+            .col-sm-auto
+              a.boton.boton--b(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
+                span Descargar
+                i.fas.fa-file-download
+    .h4.titulo-rosa Intervenciones colectivas en entornos - RPMS
+    figure.p-3.my-5
+          img(src='@/assets/curso/img27.svg', alt='Texto que describa la imagen')
+    .h4.titulo-rosa Individuales
+    p Las atenciones individuales están dirigidas a las personas en sus diferentes momentos de curso de vida y a la familia como sujeto de atención, estas intervenciones tienen como finalidad realizar detección temprana de alteraciones de salud, protección específica, diagnóstico, tratamiento, rehabilitación, paliación y educación para la salud 
+    figure.p-3.my-5
+          img(src='@/assets/curso/img28.svg', alt='Texto que describa la imagen')
+    .titulo-segundo
+      .h4.mt-5 3.3. Acciones de Gestión
+    .h4.titulo-rosa Poblacionales
+    p Los procesos de gestión de la Salud Pública definidos en la resolución 518 de 2015 definen una serie de acciones que permiten la afectación de los determinantes sociales de la salud y el logro de los resultados en salud desde la gestión sectorial, intersectorial y comunitaria, en tanto la apuesta son los resultados en salud - vía la gestión de atenciones - que requiere unos procesos de gestión.
+    figure.p-3.my-5
+        img(src='@/assets/curso/img29.svg')
+    p Son en total 13 procesos, entre los cuales se encuentran los misionales (que solo desarrolla el sector salud), estratégicos (que son desarrollados desde otros sectores, con particularidades para el sector y desde donde se potencia la interlocución con otros actores y sectores) y finalmente los procesos de apoyo. Es importante que dependiendo del proceso, la función y el relativo liderazgo de los actores cambia, aunque se mantiene la participación directa o indirecta; por tanto para la planeación integral en salud el liderazgo le corresponde a la entidad territorial de salud, mientras que en el caso de la gestión de intervenciones individuales, el asegurador con su red de prestación de servicios son los  actores principales del proceso.
+      br
+      br
+      |La articulación de los procesos y el desarrollo de las responsabilidades de los actores, es esencial para lograr brindar las intervenciones en salud definidas en la RPMS, considerando además, que el proceso de gestión de las intervenciones individuales (con participación clave de los aseguradores y su red de prestación de servicios) aporta directamente a la garantía de dichas atenciones en términos de oportunidad, disponibilidad, continuidad e integralidad.
+
+
 </template>
 
 <script>
